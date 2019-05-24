96946fe3ae14249d9b6747264c2261f829166a03
Xinran Che <gracexinran@gmail.com>
2019-05-24 00:03:43 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,12 @@
 def alphabetize(arr)
   # code here
+  a = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".split('')
+
+  arr.sort_by do |x|
+
+    x.split('').collect do |c|
+      a.index(c)
+    end
+  end
+
 end
